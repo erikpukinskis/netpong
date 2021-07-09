3cdae2c742345d323c8e888b5a4e6f994bd18b5b
Erik Pukinskis <erik@snowedin.net>
2021-07-08 21:31:22 -0600

    LSP wants versions on deno imports

--- server/start.js
+++ server/start.js
@@ -1,9 +1,9 @@
-import { serve } from "https://deno.land/std/http/server.ts";
+import { serve } from "https://deno.land/std@0.100.0/http/server.ts";
 import {
   acceptWebSocket,
   isWebSocketCloseEvent,
-} from "https://deno.land/std/ws/mod.ts";
-import { v4 } from "https://deno.land/std/uuid/mod.ts";
+} from "https://deno.land/std@0.100.0/ws/mod.ts";
+import { v4 } from "https://deno.land/std@0.100.0/uuid/mod.ts";
 
 const requests = serve({ port: 8000 });
 
