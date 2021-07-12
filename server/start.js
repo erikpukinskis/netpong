1c20403894b9d7495d496cf5979d52d849a70a83
Erik Pukinskis <erik@snowedin.net>
2021-07-11 20:46:53 -0600

    Use new proxyWebSockets function in start.js

--- server/start.js
+++ server/start.js
@@ -1,44 +1,15 @@
 import { serve } from "https://deno.land/std@0.100.0/http/server.ts";
 import {
-  acceptWebSocket,
-  isWebSocketCloseEvent,
-} from "https://deno.land/std@0.100.0/ws/mod.ts";
-import { v4 } from "https://deno.land/std@0.100.0/uuid/mod.ts";
+  SocketStore,
+  proxyWebSockets,
+  ProxyProtocol,
+} from "./websocket-proxy.ts";
 
-const requests = serve({ port: 8000 });
-
-const sockets = new Map();
+const server = serve({ port: 8000 });
+const clients = new SocketStore();
 
 console.log("Listening on http://localhost:8000");
 
-for await (const request of requests) {
-  const { conn, r: bufReader, w: bufWriter, headers } = request;
-
-  if (request.headers.get("upgrade") === "websocket") {
-    acceptWebSocket({
-      conn,
-      bufReader,
-      bufWriter,
-      headers,
-    }).then(handleWebSocketConnection);
-  } else {
-    request.respond({ body: "Hello World\n" });
-  }
-}
-
-async function handleWebSocketConnection(socket) {
-  const uuid = v4.generate();
-  sockets.set(uuid, socket);
-  console.log("socket connected");
-
-  for await (const event of socket) {
-    if (isWebSocketCloseEvent(event)) {
-      sockets.delete(uuid);
-      return;
-    }
-    if (event === "ping") {
-      console.log(event);
-      socket.send("pong!");
-    }
-  }
+for await (const request of server) {
+  proxyWebSockets(request, clients);
 }
